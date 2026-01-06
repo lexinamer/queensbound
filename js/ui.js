7fa880c3c8654a4eb8535ff0b3cb312e3b8debf6
Lexi Namer <lexinamer@gmail.com>
2026-01-06 11:56:26 -0600

    adding updated files from site download

--- js/ui.js
+++ js/ui.js
@@ -1,33 +1,69 @@
-// Initialize Tabletop to get gSheet data
-let publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1IuicD2xPSk9HibXcW42GLBsGJcKCQ4jV4cXbMi0fRT8/pubhtml';
+// Process data from File
+$(document).ready(function() {
+  var tab_name = 'Data';
+  var spreadsheet_id = '1IuicD2xPSk9HibXcW42GLBsGJcKCQ4jV4cXbMi0fRT8';
+  var api_key = 'AIzaSyCyVrmIYsa7ldGiUjkrQdHCkSkzf3NkqI8';
+  var url = 'https://sheets.googleapis.com/v4/spreadsheets/' + spreadsheet_id + '/values/' + tab_name + '?alt=json&key=' + api_key;
 
-function init() {
-  Tabletop.init({ key: publicSpreadsheetUrl,
-    callback: processData,
-    simpleSheet: true })
-};
+  $.getJSON( url, function(data) {
+      processData(data.values);
+  }) .fail(function(){
+      console.log("An error has occurred.");
+  });
+
+  // $.getJSON("assets/mapdata.json", function(data){
+  //     processData(data);
+  //     // publishPoems(data);
+  // }).fail(function(){
+  //     console.log("An error has occurred.");
+  // });
+});
 
 // Process data from gSheet
 function processData(data) {
   let poemArray = [];
   let numberSelected = 0;
+  let windowSize = $(window).width();
 
   // Push only published rows to poemArray
   for (let i = 0; i < data.length; i++) {
-    if (data[i].Published == 'Yes') {
+    if (data[i][2] == 'Yes') {
       poemArray.push(data[i]);
       numberSelected++;
-    };
-  };
+    }
+  }
 
-  if($(window).width() >= 767){
+  if(windowSize > 1024){
     processMap(poemArray);
   } else {
     processMobile(poemArray);
   }
 
+  publishPoems(poemArray);
   createModal(poemArray);
-};
+}
+
+// Publish the poems to the "Poems" page
+function publishPoems(poemArray){
+    
+  poemArray.sort((a, b) => {
+    const lastA = a[5].toLowerCase();
+    const lastB = b[5].toLowerCase();
+    return lastA.localeCompare(lastB);
+  });
+    
+  for (let i = 0; i < poemArray.length; i++) {
+      console.log(poemArray);
+    const poetFirst = poemArray[i][4];
+    const poetLast = poemArray[i][5];
+    const url = poemArray[i][8];
+    const code = poemArray[i][0];
+    
+    var poemList = `<p class="poet-${code}"><a href="${url}" target="_blank">${poetFirst} ${poetLast}</a></p>`;
+
+    $('#poems .poemscontainer').append(poemList);
+  }
+}
 
 // Match the published poems to the subway stops on map
 function processMap(poemArray) {
@@ -35,24 +71,23 @@ function processMap(poemArray) {
 
   for (let i = 0; i < poemArray.length; i++) {
     for (let x = 0; x < map.length; x++) {
-      if (map[x].id == poemArray[i].Webcode) {
+      if (map[x].id == poemArray[i][0]) {
         map.eq(x).addClass('published').attr({
           "href": "#",
           "data-featherlight": '#' + map[x].id
         });
-      };
+      }
     }
   }
 }
 
 // Publish poems for mobile view
 function processMobile(poemArray) {
-
   // Break up JSON data into objects by subway line
   let subwaylines = {};
   for (let i = 0; i < poemArray.length; i++) {
     var item = poemArray[i];
-    var x = poemArray[i].SubwayLine;
+    var x = poemArray[i][1];
     if (!(x in subwaylines)) subwaylines[x] = [];
     subwaylines[x].push(item);
   }
@@ -68,11 +103,11 @@ function processMobile(poemArray) {
 
     // Loop through the subwaylines objects to create modal triggers
     $(subwaylines[key]).each(function(){
-      let stop = this.SubwayStop;
+      let stop = this[7];
       let trigger = $(`<a>${stop}</a>`);
-      trigger.addClass('published mobile-stop').attr({
+      trigger.addClass('mobile-stop').attr({
         "href": "#",
-        "data-featherlight": '#' + this.Webcode
+        "data-featherlight": '#' + this[0]
       });
 
       // Append modal trigger to body
@@ -84,14 +119,14 @@ function processMobile(poemArray) {
 // Create Modal
 function createModal(poemArray) {
   poemArray.forEach(function(poem) {
-    let code = poem.Webcode;
-    let name = poem.Poem;
-    let poet = poem.Poet;
-    let year = poem.Year;
-    let stop = poem.SubwayStop;
-    let url = poem.SoundcloudURL;
-    let bio = poem.Bio;
-    let line = poem.SubwayLine;
+    let code = poem[0];
+    let name = poem[3];
+    let poet = poem[4] + " "+ poem[5];
+    let year = poem[6];
+    let stop = poem[7];
+    let url = poem[8];
+    let bio = poem[9];
+    let line = poem[1];
 
     var modal =
       `<div id="${code}" class="lightbox">
@@ -116,5 +151,3 @@ function createModal(poemArray) {
     $('.data').append(modal);
   });
 }
-
-$(document).ready(init);
